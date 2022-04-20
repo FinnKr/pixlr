5107ddaa875856831fa78ec708c7ea20e5c5a4cc
Emil Merle <emilmerle8@gmail.com>
2022-04-20 18:13:58 +0200

    create post fixes

---
+++ AppwriteProject/functions/Create Post/src/index.py
@@ -0,0 +1,77 @@
+import json
+import io
+from urllib.request import urlopen
+from requests.exceptions import HTTPError
+from appwrite.client import Client
+from appwrite.services.database import Database
+from appwrite.services.storage import Storage
+
+def main(req, res):
+    client = Client()
+
+    database = Database(client)
+    storage = Storage(client)
+
+    user_id = req.env.get('APPWRITE_FUNCTION_USER_ID', None)
+    bucket_id = req.env.get('APPWRITE_BUCKET_ID', None)
+
+    (
+    client
+        .set_endpoint(req.env.get('APPWRITE_FUNCTION_ENDPOINT', None))
+        .set_project(req.env.get('APPWRITE_FUNCTION_PROJECT_ID', None))
+        .set_key(req.env.get('APPWRITE_FUNCTION_API_KEY', None))
+    )
+    
+    post_collection_id = req.env.get('POSTS_COLLECTION_ID')
+
+    payload = json.loads(req.payload)
+    title = payload["title"]
+    image64 = payload["image64"]
+
+    if not image64 or not title:
+        obj = {
+            "error": "title and image64 should not be empty",
+            "statusCode": 400
+        }
+        return res.json(obj)
+
+
+    imageRes = urlopen(image64)
+
+    #output = io.BytesIO()
+    #output.write(imageRes.file.read())
+
+    #with open("image.png", "wb") as f:
+    #    f.write(imageRes.file.read())
+
+    result = storage.create_file(bucket_id, "unique()", imageRes.file.read())
+
+    if not result:
+        obj = {
+            "error": "error while creating document",
+            "statusCode": 500
+        }
+        return res.json(obj)
+
+
+    image_id = result["$id"]
+
+    post = {
+        "title": title,
+        "image_id": image_id,
+        "user_id": user_id
+    }
+
+    result = database.create_document(post_collection_id, "unique()", post)
+
+    if not result:
+        obj = {
+            "error": "error while creating document",
+            "statusCode": 500
+        }
+        return res.json(obj)
+
+
+    result["statusCode"] = 201
+
+    return res.json(result)
